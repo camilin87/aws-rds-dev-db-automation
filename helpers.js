362ccaf80544f0bdff2e82bd351c2a430bd298ba
Camilo Sanchez <camilin87@gmail.com>
2019-02-23 19:11:09 -0500

    create a helper to specific configuration sections

--- helpers.js
+++ helpers.js
@@ -1,4 +1,7 @@
+const fs = require('fs')
+
 module.exports = {
     sortRdsSnapshot: (a, b) => a.SnapshotCreateTime > b.SnapshotCreateTime ? -1 : 1,
-    todayString: () => new Date().toISOString().split('T')[0]
+    todayString: () => new Date().toISOString().split('T')[0],
+    readConfigModule: (name) => JSON.parse(fs.readFileSync('./config.json', 'utf8'))[name]
 }
