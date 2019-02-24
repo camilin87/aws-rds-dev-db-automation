a37f0aeb996f617450869717c3ee8ba0db3ed386
Camilo Sanchez <camilin87@gmail.com>
2019-02-24 17:40:50 -0500

    ignore the scrap folder

--- helpers.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-
-module.exports = {
-    sortRdsSnapshot: (a, b) => a.SnapshotCreateTime > b.SnapshotCreateTime ? -1 : 1,
-    todayString: () => new Date().toISOString().split('T')[0]
-}
---
+++ main.js
@@ -0,0 +1,43 @@
+const rfr = require('rfr')
+const configHelper = rfr('lib/config')
+const rdsService = require('aws-automation-utils').rds
+const sanitizeService = rfr('lib/sanitize')
+const updateConnectionStringService = rfr('lib/updateConnectionString')
+
+const main = async () => {
+    try {
+        console.log('Program Starting...')
+        const config = configHelper.read()
+
+        var restoreConfig = config.restore
+        const newDatabaseName = await rdsService.restore(restoreConfig)
+
+        var connectionStringConfig = config.connectionString
+        if (newDatabaseName){
+            connectionStringConfig.dbIdentifier = newDatabaseName
+        }
+
+        var changePasswordConfig = config.updateMasterPassword
+        changePasswordConfig.dbIdentifier = connectionStringConfig.dbIdentifier
+        await rdsService.updatePassword(changePasswordConfig)
+
+        var sanitizeConfig = config.sanitizeDevDatabase
+        sanitizeConfig.connectionString = connectionStringConfig
+        await sanitizeService.sanitize(sanitizeConfig)
+
+        var deleteOlderConfig = config.deleteOldDevDatabase
+        await rdsService.deleteOlder(deleteOlderConfig)
+
+        var updateConnectionStringConfig = config.updateConnectionString
+        updateConnectionStringConfig.connectionString = connectionStringConfig
+        await updateConnectionStringService.updateConnectionString(updateConnectionStringConfig)
+
+        console.log('Ending...')
+        process.exit(0)
+    } catch(err){
+        console.log('ERROR', err)
+        process.exit(1)
+    }
+}
+
+main()
